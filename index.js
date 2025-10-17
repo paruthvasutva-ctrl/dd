54aec8ad67bd85dd4b5026d468bd9a406956fdda
paruthvasutva-ctrl <paruthvasutva@gmail.com>
2025-10-17 17:58:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧", "🎮"];
+const statusMessages = ["🎧", "Watching All Of You"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
